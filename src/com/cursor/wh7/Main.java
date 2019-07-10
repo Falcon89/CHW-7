80c3f63c63cbafdc34668eeb8cd6530d4f4a99b7
falcon89 <vasyl.kachala@gmail.com>
2019-07-10 14:19:28 +0300

    add first class

---
+++ src/com/cursor/wh7/Main.java
@@ -0,0 +1,10 @@
+package com.cursor.wh7;
+
+import static com.cursor.wh7.AssemblyOfSongs.menuCase;
+
+public class Main {
+
+    public static void main(String[] args) {
+        menuCase();
+    }
+}
