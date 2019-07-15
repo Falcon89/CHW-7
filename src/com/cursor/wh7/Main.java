f30afb4d1a3a67cb19d064b30a837cb77f2ca585
falcon89 <vasyl.kachala@gmail.com>
2019-07-15 15:39:21 +0300

    add change

--- src/com/cursor/wh7/Main.java
+++ src/com/cursor/wh7/Main.java
@@ -1,9 +1,9 @@
 package com.cursor.wh7;
 
 import java.io.IOException;
-import static com.cursor.wh7.СhooseMenu.menuCase;
+import static com.cursor.wh7.ServiceUtils.caseUtils;
 public class Main {
     public static void main(String[] args) throws IOException {
-        menuCase();
+        caseUtils();
     }
 }
--- src/com/cursor/wh7/ServiceUtils.java
+++ src/com/cursor/wh7/ServiceUtils.java
@@ -3,25 +3,25 @@ package com.cursor.wh7;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class СhooseMenu {
+public class ServiceUtils {
     /**
-     * Created method menuCase described options
+     * Created method caseUtils described options
      * @throws IOException
      */
-    public static void menuCase() throws IOException {
+    public static void caseUtils() throws IOException {
         System.out.println("*************Songs Option*************");
         System.out.println("1 Calculate the total number of s in the text");
         System.out.println("2 Inappropriate s or s that are less than 3 characters long will not be taken into account");
         System.out.println("3 Calculate the number of s that need to be excluded");
         System.out.println("4 All the most common (repeated) s in decreasing order");
         System.out.println("*************End Songs Option*************");
-        switchCase();
+        switchUtils();
     }
     /**
-     * Created method switchCase described switch
+     * Created method switchUtils described switch
      * @throws IOException
      */
-    public static void switchCase() throws IOException {
+    public static void switchUtils() throws IOException {
         ExecutingMenuMethods executingMenuMethods = new ExecutingMenuMethods();
         Scanner scanner = new Scanner(System.in);
         int switchCase = scanner.nextInt();
